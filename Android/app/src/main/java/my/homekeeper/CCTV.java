3a040516ab3594544c705abaf217ec7db2fafdc1
psy1064 <psy1064@naver.com>
2019-08-27 23:14:21 +0900

    CCTV fragment 수정

--- Android/app/src/main/java/my/homekeeper/CCTV.java
+++ Android/app/src/main/java/my/homekeeper/CCTV.java
@@ -1,8 +1,5 @@
 package my.homekeeper;
 
-
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,16 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.widget.ImageView;
-import android.widget.ViewFlipper;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 
 public class CCTV extends Fragment {
     WebView webView;
@@ -28,13 +15,15 @@ public class CCTV extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cctv, container, false);
 
-        webView = (WebView) view.findViewById(R.id.cctvweb);
+        webView = (WebView) view.findViewById(R.id.cctvWeb);
         webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
-        webView.loadData("<html><head><style type='text/css'>body{margin:auto auto;text-align:center;} img{width:100%25;} div{overflow: hidden;} </style></head>" +
-                        "<body><div><img src='http://14.51.11.214:8082/'/></div></body></html>" ,
-                "text/html",  "UTF-8");
+        webView.loadData("<html><head><style type='text/css'>body{margin:auto auto;text-align:center;} " +
+                        "img{width:100%25;} div{overflow: hidden;} </style></head>" +
+                        "<body><div><img src='http://121.153.150.18:8080/'/></div></body></html>",
+                "text/html", "UTF-8");
+
 
         return view;
     }
