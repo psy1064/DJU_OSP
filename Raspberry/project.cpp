be9c500400d0037fb78e9862673897ffac825c2f
psy1064 <psy1064@naver.com>
2019-09-23 23:31:16 +0900

    소스파일 헤더파일 분리

--- Raspberry/project.cpp
+++ Raspberry/project.cpp
@@ -1,225 +1,29 @@
-#include<iostream>
-#include<cstdio>
-#include<errno.h>
-#include<string.h>
-#include<stdlib.h>
-#include<signal.h>
-#include<wiringPi.h>
-#include<wiringSerial.h>
-
-#define DHT11 0			// DHT11 wPi Pin 0, BGM 17
-#define HUMAN 1			// Human detect sensor wPi Pin 1, BGM 18
-#define LED_G 2			// LED wPi pin 2, BGM 2 7
-#define LED_R 3
+#include "process.h"
+#include <wiringPi.h>
+#include <iostream>
+#include <signal.h>
 
 #define DELAY 5000		// Delay 5초
-
-using namespace std;
-
-unsigned char exit_flag = 0;
-unsigned int humanCount = 0;
-
-class Project {
-private:
-	uint8_t dhtData[5];			// DHT11 센서값 배열
-	int hum, temp;				// 습도, 온도
-
-	int fd;						// 시리얼통신 정보
-	uint8_t pms[32];			// PMS7003 센서값 배열
-	uint16_t pm1, pm25, pm10;	// 극초미세먼지, 초미세먼지, 미세먼지 값
-	uint16_t checkcode;			// PMS7003 Checkcode 값
-
-public:
-	Project();
-	// Constructor
-	~Project();
-	void DHTProcess();
-	void DHTSend();
-	void DHTResponse();
-	bool DHTGetDate();
-	// DHT11 method
-	void PMSRecive();
-	// PMS7003 method
-	void ProjectProcess();
-	void PrintData();
-	// Whole Process method
-};
-
-Project::Project()
-{
-	system("sudo motion");		// Motion Enable
-	cout << "CCTV Enable" << endl;
-	hum = temp = 0;
-	for (int i = 0; i < 5; i++)
-	{
-		dhtData[i] = 0;
-	}
-	if (wiringPiSetup() == -1)
-	{
-		cout << "wiringPiSetup Error" << endl;
-		exit(1);
-	}
-	else cout << "wiringPiSetup" << endl;
-	if ((fd = serialOpen("/dev/ttyUSB0", 9600)) < 0)
-	{
-		fprintf(stderr, "Unable serial", strerror(errno));
-		exit(1);
-	} // 시리얼 통신 초기화
-
-	pinMode(LED_G, OUTPUT);
-	pinMode(LED_R, OUTPUT);
-	pinMode(HUMAN, INPUT);
-	digitalWrite(LED_R, LOW);
-	digitalWrite(LED_G, HIGH);
-}
-Project::~Project()
-{
-	system("killall -15 motion");
-	cout << "CCTV disabled" << endl;
-	digitalWrite(LED_G, LOW);
-}
-void Project::DHTProcess()
-{
-	DHTSend();
-	DHTResponse();
-	bool tmp = DHTGetDate();
-} // DHT11 센서 측정 함수
-void Project::DHTSend()
-{
-	pinMode(DHT11, OUTPUT);
-	digitalWrite(DHT11, LOW);
-	delay(20);
-	digitalWrite(DHT11, HIGH);
-	delayMicroseconds(30);
-}
-void Project::DHTResponse()
-{
-	pinMode(DHT11, INPUT);
-
-	while (digitalRead(DHT11) != LOW);
-	while (digitalRead(DHT11) != HIGH);
-	while (digitalRead(DHT11) != LOW);
-}
-bool Project::DHTGetDate()
-{
-	int count = 0;
-	int j;
-	for (j = 0; j < 40; j++)
-	{
-		count = 0;
-		while (digitalRead(DHT11) == LOW)
-		{
-			delayMicroseconds(1);
-			count++;
-			if (count >= 255) return false; 	// start to transmit 1-bit data(50us)
-		} // When DHT is sending data to MCU, every bit of data begins with the 50us low-voltage-level
-		count = 0;
-		while (digitalRead(DHT11) == HIGH)
-		{
-			delayMicroseconds(1);
-			count++;
-			if (count >= 255) return false;
-		}
-		dhtData[j / 8] <<= 1;
-		if (count > 30)
-		{
-			dhtData[j / 8] |= 1;
-		}
-	}
-	if ((j >= 40) && (dhtData[4] == dhtData[0] + dhtData[1] + dhtData[2] + dhtData[3]))
-	{
-		hum = (int)dhtData[0];
-		temp = (int)dhtData[2];
-		cout << "DHT11 Right" << endl;
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-void Project::PMSRecive()
-{
-	for (int i = 0; i < 32; i++)
-	{
-		pms[i] = serialGetchar(fd);
-		if (i < 30)
-			checkcode += pms[i];
-	}
-	if ((pms[0] == 0x42) && (pms[1] == 0x4d))
-	{
-		uint16_t tmp = (pms[30] << 8) + pms[31];
-		if (checkcode == tmp)	cout << "PMS7003 Right" << endl;
-
-		pm1 = (pms[10] << 8) + pms[11];
-		pm25 = (pms[12] << 8) + pms[13];
-		pm10 = (pms[14] << 8) + pms[15];
-
-		checkcode = 0;
-	}
-}
-void Project::PrintData()
-{
-	cout << "============================" << endl;
-	cout << "hum = " << hum << " temp = " << temp << endl;
-	cout << "PM1 = " << pm1 << " PM2.5 = " << pm25 << " PM10 = " << pm10 << endl;
-	cout << "============================" << endl;
-}
-void Project::ProjectProcess()
-{
-	DHTProcess();
-	PMSRecive();
-	PrintData();
-}
-void humanInterrupt()
-{
-	humanCount++;
-	cout << "Human detect" << endl;
-	cout << "Human count = " << humanCount << endl;
-}
-
-void signal_handler(int signo)
-{
-	cout << "call signal handler" << endl;
-	system("sudo killall -15 motion");
-	exit_flag = 1;
-	digitalWrite(LED_G, LOW);
-	digitalWrite(LED_R, HIGH);
-
-	exit(0);
-}
-
-void call_exitfunc()
-{
-	cout << "call_ateixt func" << endl;
-	if (exit_flag == 0)
-	{
-		system("sudo killall -15 motion");
-		digitalWrite(LED_G, LOW);
-		digitalWrite(LED_R, HIGH);
-	}
-	cout << "CCTV disabled" << endl;
-} // 시스템 종료 시 호출되는 함수
+#define HUMAN 1			// Human detect sensor wPi Pin 1, BGM 18
 
 int main()
 {
+	PROCESS process;
+
 	signal(SIGINT, signal_handler);
 	signal(SIGTERM, signal_handler);
 
 	atexit(call_exitfunc);
 
-	Project project;
-
 	if (wiringPiISR(HUMAN, INT_EDGE_RISING, &humanInterrupt) < 0)
 	{
-		return 1;
+		exit(1);
 	}
-	else cout << "Interrupt enabled" << endl;
-
+	else std::cout << "Interrupt enabled\n";
 
 	while (1)
 	{
-		project.ProjectProcess();
+		process.processCycle();
 		delay(DELAY);
 	}
 }
