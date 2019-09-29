e39c172ece041f82d4afcd39712baed8bc6faeb0
psy1064 <psy1064@naver.com>
2019-09-29 22:19:47 +0900

    로딩 화면 구현 + 앱 로고 추가

---
+++ Android/app/src/main/java/my/homekeeper/loadingActivity.java
@@ -0,0 +1,29 @@
+package my.homekeeper;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class loadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        startLoading();
+    }
+
+    void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
