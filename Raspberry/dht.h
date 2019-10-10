ea302d6573ca0c2ce7213dcccadd0c35c8307f6a
psy1064 <psy1064@naver.com>
2019-10-11 00:10:16 +0900

    dht11 source code

--- Raspberry/dht.h
+++ Raspberry/dht.h
@@ -1,11 +1,20 @@
 #ifndef DHT_H
 #define DHT_H
 
+#include <stdint.h>
 
-class dht
-{
+class DHT {
+private:
+    uint8_t dhtData[5];			// DHT11 ï¿œï¿œï¿œï¿œï¿œï¿œ ï¿œè¿­
+    int hum, temp;				// ï¿œï¿œï¿œï¿œ, ï¿œÂµï¿œ
 public:
-    dht();
+    DHT();
+    void DHTProcess();
+    void DHTSend();
+    void DHTResponse();
+    bool DHTGetDate();
+    int getTemp();
+    int getHum();
 };
 
 #endif // DHT_H
