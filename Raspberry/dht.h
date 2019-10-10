4614440e3b8cf5a696b80cfa5fe17c46426873c3
psy1064 <psy1064@naver.com>
2019-10-11 00:08:16 +0900

    GUI

--- Raspberry/dht.h
+++ Raspberry/dht.h
@@ -1,19 +1,11 @@
 #ifndef DHT_H
 #define DHT_H
 
-#include <stdint.h>
 
-class DHT {
-private:
-	uint8_t dhtData[5];			// DHT11 센서값 배열
-	int hum, temp;				// 습도, 온도
+class dht
+{
 public:
-	DHT();
-	void DHTProcess();
-	void DHTSend();
-	void DHTResponse();
-	bool DHTGetDate();
-	int getTemp();
-	int getHum();
+    dht();
 };
-#endif
+
+#endif // DHT_H
--- Raspberry/pms.cpp
+++ Raspberry/pms.cpp
@@ -1,40 +1,6 @@
 #include "pms.h"
-#include <errno.h>
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <wiringPi.h>
-#include <wiringSerial.h>
 
-PMS::PMS()
+pms::pms()
 {
-	if ((fd = serialOpen("/dev/ttyUSB0", 9600)) < 0)
-	{
-		fprintf(stderr, "Unable serial", strerror(errno));
-		exit(1);
-	} // 시리얼 통신 초기화
-}
-void PMS::PMSReceive()
-{
-	for (int i = 0; i < 32; i++)
-	{
-		pms[i] = serialGetchar(fd);
-		if (i < 30)
-			checkcode += pms[i];
-	}
-	if ((pms[0] == 0x42) && (pms[1] == 0x4d))
-	{
-		uint16_t tmp = (pms[30] << 8) + pms[31];
-		if (checkcode == tmp)	std::cout << "PMS7003 Right\n";
 
-		pm1 = (pms[10] << 8) + pms[11];
-		pm25 = (pms[12] << 8) + pms[13];
-		pm10 = (pms[14] << 8) + pms[15];
-			   	
-		checkcode = 0;
-	}
-}
-int PMS::getPM()
-{
-	return pm1;
 }
