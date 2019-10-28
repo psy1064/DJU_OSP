1361c6687d5728cbb0fa3a06f8fe2bb8910271fb
psy1064 <psy1064@naver.com>
2019-10-28 12:01:41 +0900

    main cpp 파일

--- Raspberry/main.cpp
+++ Raspberry/main.cpp
@@ -8,8 +8,8 @@
 #include <pthread.h>
 #include <QString>
 
-#define DELAY 5000		// Delay 5ÃÊ
-#define HUMAN 1			// Human detect sensor wPi Pin 1, BGM 18
+#define DELAY 5000		// Delay 5초 
+#define HUMAN 1			// Human detect sensor wPi Pin 1
 
 PROCESS process;
 
@@ -21,21 +21,20 @@ void* loopFunction(void* num)
     Widget widget;
     while (1)
     {
-        widget.show();
-        process.processCycle();
+        widget.show();			// Qt display
+        process.processCycle();		// 데이터 수집
         process.putData(hum, temp, dust);
-        widget.setData(hum, temp, dust);
         delay(DELAY);
     }
 }
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication a(argc, argv);		// QApplication 생성
     /*Widget widget;
     widget.show();*/
 
-    pthread_t loopThread;
+    pthread_t loopThread;		// 데이터 수집 및 Qt 출력 쓰레드
     pthread_create(&loopThread, NULL, loopFunction, NULL);
 
     return a.exec();
