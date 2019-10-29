7b1647b20306898357bb15e03d255b02a8d34839
psy1064 <psy1064@naver.com>
2019-10-30 03:11:47 +0900

    sensor value display thread

--- Raspberry/processthread.cpp
+++ Raspberry/processthread.cpp
@@ -1,6 +1,19 @@
 #include "processthread.h"
-
-processThread::processThread()
+#include "process.h"
+#include "dialog.h"
+processThread::processThread(QObject *parent) : QThread(parent)
 {
+
+}
+void processThread::run()
+{
+    int t, h, d;
+    while(1)
+    {
+        process.processCycle();
+        process.putData(&t, &h, &d);
+        emit setValue(t, h, d);
+        sleep(3);
+    }
 
 }
--- Raspberry/processthread.h
+++ Raspberry/processthread.h
@@ -1,11 +1,20 @@
 #ifndef PROCESSTHREAD_H
 #define PROCESSTHREAD_H
 
+#include <QThread>
+#include "process.h"
 
-class processThread
+class processThread : public QThread
 {
+    Q_OBJECT
 public:
-    processThread();
+    explicit processThread(QObject *parent = 0);
+
+private:
+    PROCESS process;
+    void run();
+signals:
+    void setValue(int temp, int hum, int dust);
 };
 
 #endif // PROCESSTHREAD_H
