ce28a4d331f8a3dd00d5fc11854fa99cecb0d496
psy1064 <psy1064@naver.com>
2019-11-05 16:10:55 +0900

    textLabel 글꼴 및 정렬

--- Raspberry/dialog.cpp
+++ Raspberry/dialog.cpp
@@ -7,15 +7,21 @@ Dialog::Dialog(QWidget *parent) :
     ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+    ui->tempValue->setFont(QFont("FibotoLt",20));
+    ui->humValue->setFont(QFont("FibotoLt",20));
+    ui->dustValue->setFont(QFont("FibotoLt",20));
     pthread = new processThread(this);
     connect(pthread, SIGNAL(setValue(int, int, int)), this, SLOT(showValue(int, int, int)));
     pthread->start();
 }
 void Dialog::showValue(int temp, int hum, int dust)
 {
-    ui->tempValue->setText(QString::number(temp));
-    ui->humValue->setText(QString::number(hum));
-    ui->dustValue->setText(QString::number(dust));
+    ui->tempValue->setText(QString::number(temp) + "°C");       // tempValue 라벨 텍스트 설정
+    ui->tempValue->setAlignment(Qt::AlignCenter);               // 가운데 정렬
+    ui->humValue->setText(QString::number(hum) + '%');
+    ui->humValue->setAlignment(Qt::AlignCenter);
+    ui->dustValue->setText(QString::number(dust) + "㎍/㎥");
+    ui->dustValue->setAlignment(Qt::AlignCenter);
 }
 Dialog::~Dialog()
 {
