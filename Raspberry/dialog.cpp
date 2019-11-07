23247518ccb3f85fac0f9ce708a32e96325e09de
psy1064 <psy1064@naver.com>
2019-11-07 13:44:58 +0900

    출력 화면 아이콘 추가 및 코드 수정

--- Raspberry/dialog.cpp
+++ Raspberry/dialog.cpp
@@ -1,7 +1,7 @@
 #include "dialog.h"
 #include "ui_dialog.h"
 #include "processthread.h"
-#include <QImage>
+//#include <QImage>
 #include <QPixmap>
 
 Dialog::Dialog(QWidget *parent) :
@@ -13,31 +13,26 @@ Dialog::Dialog(QWidget *parent) :
     red.setColor(QPalette::Active, QPalette::WindowText, Qt::red);
     yellow.setColor(QPalette::Active, QPalette::WindowText, Qt::yellow);
     blue.setColor(QPalette::Active, QPalette::WindowText, Qt::blue);
-    green.setColor(QPalette::Active, QPalette::WindowText, Qt::green);
+    green.setColor(QPalette::Active, QPalette::WindowText, Qt::green);      	// Palette에 색 설정
 
     tempIconPicture.load("/home/pi/Github/DJU_OSP/Raspberry/temperature.png");
     humIconPicture.load("/home/pi/Github/DJU_OSP/Raspberry/hum.png");
-    dustIconPicture.load("/home/pi/Github/DJU_OSP/Raspberry/dust.png");
+    dustIconPicture.load("/home/pi/Github/DJU_OSP/Raspberry/dust.png");		// Icon에 넣을 사진 경로 지정
 
     ui->tempIcon->setPixmap(tempIconPicture.scaled(128,128,Qt::KeepAspectRatio));
     ui->humIcon->setPixmap(humIconPicture.scaled(128,128,Qt::KeepAspectRatio));
-    ui->dustIcon->setPixmap(dustIconPicture.scaled(128,128,Qt::KeepAspectRatio));
+    ui->dustIcon->setPixmap(dustIconPicture.scaled(128,128,Qt::KeepAspectRatio));	// Icon에 사진 넣기
 
     ui->tempIcon->setAlignment(Qt::AlignCenter);
     ui->humIcon->setAlignment(Qt::AlignCenter);
-    ui->dustIcon->setAlignment(Qt::AlignCenter);
+    ui->dustIcon->setAlignment(Qt::AlignCenter);				// 사진 가운데 정렬
 
-    /*Image img("/home/pi/Github/DJU_OSP/Raspberry/dust.png");
-    QPixmap buf = QPixmap::fromImage(img);
-
-    ui->dustIcon->setPixmap(buf);
-    ui->dustIcon->resize(buf.width(), buf.height());
-*/
     ui->tempValue->setFont(QFont("FibotoLt",20));
     ui->humValue->setFont(QFont("FibotoLt",20));
-    ui->dustValue->setFont(QFont("FibotoLt",20));
-    pthread = new processThread(this);
-    connect(pthread, SIGNAL(setValue(int, int, int)), this, SLOT(showValue(int, int, int)));
+    ui->dustValue->setFont(QFont("FibotoLt",20));	// 각 센세 데이터 출력 값 폰트, 글자 크기 설정
+
+    pthread = new processThread(this);			// 센서 데이터 수집 쓰레드 동적 할당
+    connect(pthread, SIGNAL(setValue(int, int, int)), this, SLOT(showValue(int, int, int)));	// 센서 데이터 수집 쓰레드의 setValue 함수에서 값을 읽어와 showValue에 넣음
     pthread->start();
 }
 void Dialog::showValue(int temp, int hum, int dust)
